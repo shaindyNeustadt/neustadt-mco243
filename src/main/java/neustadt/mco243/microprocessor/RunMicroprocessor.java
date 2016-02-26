9d51a08819a14c3de29249d197c5454a31ead5aa
Gen Student1 <student1@student.touro.edu>
2016-02-25 22:24:18 -0500

    Ran microprocessor from the command prompt

--- src/main/java/neustadt/mco243/microprocessor/RunMicroprocessor.java
+++ src/main/java/neustadt/mco243/microprocessor/RunMicroprocessor.java
@@ -3,14 +3,16 @@ package neustadt.mco243.microprocessor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class RunMicroprocessor {
 
 	public static void main(String[] args) {
 
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(
-					"./mach.in"));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			// BufferedReader reader = new BufferedReader(new FileReader(
+			// "./mach.in"));
 
 			String nextLine;
 			while ((nextLine = reader.readLine()) != null) {
